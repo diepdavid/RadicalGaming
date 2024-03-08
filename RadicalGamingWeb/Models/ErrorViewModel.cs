03b2e0aa0d4b7f01f64b3fa6cfc3a3d9366f6b35
diepdavid <diepdavid5@gmail.com>
2024-03-08 13:13:40 +0100

    Add project files.

---
+++ RadicalGamingWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RadicalGamingWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
